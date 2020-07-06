8599bbd3c7440f078b7ed24a1c986bb1abacd9ec
Zack Lorenzzo Corrêa <zackluthier@hotmail.com>
2020-07-06 18:39:40 -0300

    addedd more MVPS

--- timer.js
+++ timer.js
@@ -25,8 +25,26 @@ function clock(deathHour, deathMinute, spawnTime, div) {
 }
 
 mvpMap = {
+<<<<<<< Updated upstream
     "amonra" : (deathHour, deathMinute) => clock(deathHour, deathMinute, '1', "#amonra-time"),
     "abelhaRainha" : (deathHour, deathMinute) => clock(deathHour, deathMinute, '2', "#abelhaRainha-time")
+=======
+    amonra: (deathHour, deathMinute) => clock(deathHour, deathMinute, '1', "#amonra-time", "amonra"),
+    mjolnir_04: (deathHour, deathMinute) => clock(deathHour, deathMinute, '2', "#mjolnir_04-time", "mjolnir_04"),
+    gld_dun02: (deathHour, deathMinute) => clock(deathHour, deathMinute, '8', "#gld_dun02-time", "gld_dun02"),
+    amonraPesadelo: (deathHour, deathMinute) => clock(deathHour, deathMinute, '1', "#amonraPesadelo-time", "amonraPesadelo"),
+    ra_fild03: (deathHour, deathMinute) => clock(deathHour, deathMinute, '3', "#ra_fild03-time", "ra_fild03"),
+    ra_fild04: (deathHour, deathMinute) => clock(deathHour, deathMinute, '5', "#ra_fild04-time", "ra_fild04"),
+    ve_fild01: (deathHour, deathMinute) => clock(deathHour, deathMinute, '3', "#ve_fild01-time", "ve_fild01"),
+    ve_fild02: (deathHour, deathMinute) => clock(deathHour, deathMinute, '6', "#ve_fild02-time", "ve_fild02"),
+    gld_dun03_2: (deathHour, deathMinute) => clock(deathHour, deathMinute, '8', "#gld_dun03_2-time", "gld_dun03_2"),
+    aprendiz: (deathHour, deathMinute) => clock(deathHour, deathMinute, '3', "#aprendiz-time", "aprendiz"),
+    prt_maze03: (deathHour, deathMinute) => clock(deathHour, deathMinute, '3', "#prt_maze03-time", "prt_maze03"),
+    gld_dun03: (deathHour, deathMinute) => clock(deathHour, deathMinute, '8', "#gld_dun03-time", "gld_dun03"),
+    bafoAmaldicoado: (deathHour, deathMinute) => clock(deathHour, deathMinute, '2', "#bafoAmaldicoado-time", "bafoAmaldicoado"),
+    belzebu: (deathHour, deathMinute) => clock(deathHour, deathMinute, '12', "#belzebu-time", "belzebu"),
+    gtb: (deathHour, deathMinute) => clock(deathHour, deathMinute, '1', "#gtb-time", "gtb"),
+>>>>>>> Stashed changes
 };
 
 function init(){
@@ -34,8 +52,15 @@ function init(){
     mvpMap["abelhaRainha"](11,0);
 }
 
+<<<<<<< Updated upstream
 
 $(document).ready(init());
 
+=======
+function init() {
+    let mvpTimers = Object.values(mvpMap);
+    mvpTimers.forEach((value) => value())
+}
+>>>>>>> Stashed changes
 
 
